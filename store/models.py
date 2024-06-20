6c7875b9df3dd2696b5fccbb590949f065f33a2d
Dobromir Kovachev <dobromir.mail@gmail.com>
2024-06-20 08:33:48 +0300

    Adding models for 'store' app

--- store/models.py
+++ store/models.py
@@ -1,3 +1,73 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)             # Always to be used for monetary values 9999.99
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)                       # Django will automatically stores the current datetime here
+    collection = models.ForeignKey('Collection', on_delete=models.PROTECT)  # if you delete collection to NOT delete all the products in thet collection
+
+
+class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
+
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed')
+    ]
+
+    placed_at = models.DateTimeField(auto_now_add=True)         # first time we create an Order django automatically populates this field 
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    customer = models.ForeignKey('Customer', on_delete=models.PROTECT)    # if we accidently delete a customer to not delete the orders
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey('Order', on_delete=models.PROTECT)        # if we delete an Order to not delete OrderItem
+    product = models.ForeignKey('Product', on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)    # price of products can change over time so we should always store the price
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.OneToOneField('Customer', on_delete=models.CASCADE, primary_key=True)     # when we delete a customer associated address will also be deleted (if SET_NULL then address will not be deleted from database)
+
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey('Cart', on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)    # if we delete a product, that product should be deleted from all the existing shopping carts as well
+    quantity = models.PositiveSmallIntegerField()
+
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+
