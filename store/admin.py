cc45e0e39937cf0bd6d09c1205cedb4e78d61665
Dobromir Kovachev <dobromir.mail@gmail.com>
2024-06-23 10:38:43 +0300

    Added sessions in installed_apps and all models in the admin panel

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Promotion, Collection, Cart, Customer, Product, Order, OrderItem, Address, CartItem
 
-# Register your models here.
+
+admin.site.register(Promotion)
+admin.site.register(Collection)
+admin.site.register(Cart)
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Address)
+admin.site.register(CartItem)
--- tags/admin.py
+++ tags/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tag, TaggedItem
+
+admin.site.register(Tag)
+admin.site.register(TaggedItem)
