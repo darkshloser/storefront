e00df737939e0f304845f9718c04bd4c3df9568f
Dobromir Kovachev <dobromir.mail@gmail.com>
2024-06-23 09:17:32 +0300

    Prepare the models for Store, Tags and Likes

--- likes/models.py
+++ likes/models.py
@@ -1,17 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
 
-class Tag(models.Model):
-    lebel = models.CharField(max_length=255)
-
-
-class TaggedItem(models.Model):
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-    # Type (product, video, article)
-    # ID
+class LikedItems(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
+
 
--- store/models.py
+++ store/models.py
@@ -7,14 +7,14 @@ class Promotion(models.Model):
     # field 'product_set' will be created where it will return all the products particular promotion is aplied to 
 
 
-class Product(models.Model):
+class Collection(models.Model):
     title = models.CharField(max_length=255)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)             # Always to be used for monetary values 9999.99
-    inventory = models.IntegerField()
-    last_update = models.DateTimeField(auto_now=True)                       # Django will automatically stores the current datetime here
-    collection = models.ForeignKey('Collection', on_delete=models.PROTECT)  # if you delete collection to NOT delete all the products in thet collection
-    promotions = models.ManyToManyField(Promotion, related_name='product_set')  # Django is going to create reverse relationship in the Promotion class
+    feature_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')  # if we specify related_name='+' this tells Django not to create reverce relationship
+
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
@@ -35,6 +35,16 @@ class Customer(models.Model):
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
 
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)                    # Always to be used for monetary values 9999.99
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)                           # Django will automatically stores the current datetime here
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)        # if you delete collection to NOT delete all the products in thet collection
+    promotions = models.ManyToManyField(Promotion, related_name='product_set')  # Django is going to create reverse relationship in the Promotion class
+
+
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
     PAYMENT_STATUS_COMPLETE = 'C'
@@ -48,12 +58,12 @@ class Order(models.Model):
 
     placed_at = models.DateTimeField(auto_now_add=True)         # first time we create an Order django automatically populates this field 
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
-    customer = models.ForeignKey('Customer', on_delete=models.PROTECT)    # if we accidently delete a customer to not delete the orders
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)    # if we accidently delete a customer to not delete the orders
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey('Order', on_delete=models.PROTECT)        # if we delete an Order to not delete OrderItem
-    product = models.ForeignKey('Product', on_delete=models.PROTECT)
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)        # if we delete an Order to not delete OrderItem
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
     quantity = models.PositiveSmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)    # price of products can change over time so we should always store the price
 
@@ -61,19 +71,13 @@ class OrderItem(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
-    customer = models.OneToOneField('Customer', on_delete=models.CASCADE, primary_key=True)     # when we delete a customer associated address will also be deleted (if SET_NULL then address will not be deleted from database)
-
-
-class Cart(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)     # when we delete a customer associated address will also be deleted (if SET_NULL then address will not be deleted from database)
 
 
 class CartItem(models.Model):
-    cart = models.ForeignKey('Cart', on_delete=models.CASCADE)
-    product = models.ForeignKey('Product', on_delete=models.CASCADE)    # if we delete a product, that product should be deleted from all the existing shopping carts as well
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)    # if we delete a product, that product should be deleted from all the existing shopping carts as well
     quantity = models.PositiveSmallIntegerField()
 
 
-class Collection(models.Model):
-    title = models.CharField(max_length=255)
 
