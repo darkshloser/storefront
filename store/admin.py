5a5cb2831f16e4e3844990f25b162c6ad249b915
Dobromir Kovachev <dobromir.mail@gmail.com>
2024-07-01 03:28:38 +0300

    Add 'computed column' in admin panel for products

--- store/admin.py
+++ store/admin.py
@@ -11,9 +11,16 @@ admin.site.register(Collection)
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price']
+    list_display = ['title', 'unit_price', 'inventory_status']
     list_editable = ['unit_price']
     list_per_page = 10
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product: Product):
+        if product.inventory < 10:
+            return 'Low'
+        return 'OK'
+
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
