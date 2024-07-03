bb24cb076a33e57dce0bf5a485dff364f484b8fe
Dobromir Kovachev <dobromir.mail@gmail.com>
2024-07-03 10:39:07 +0300

    Use MOdelSerializers basig usage

--- store/serializers.py
+++ store/serializers.py
@@ -8,16 +8,19 @@ from rest_framework import serializers
 #     title = serializers.CharField(max_length=255)
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
-    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    # collection = CollectionSerializer()
-    collection = serializers.HyperlinkedRelatedField(
-        queryset=Collection.objects.all(),
-        view_name='collection-detail'
-    )   #  "collection": "http://127.0.0.1:8000/store/coections/3/"
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'collection']  # by default Model serializer use primary key related fields
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
+    # price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+    # price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    # # collection = CollectionSerializer()
+    # collection = serializers.HyperlinkedRelatedField(
+    #     queryset=Collection.objects.all(),
+    #     view_name='collection-detail'
+    # )   #  "collection": "http://127.0.0.1:8000/store/coections/3/"
 
     def calculate_tax(self, product: Product) -> Decimal:
         return product.unit_price * Decimal(1.1)
