3e05ce06a6f667e7a177124abcf3c97e8dc1c4c5
Dobromir Kovachev <dobromir.mail@gmail.com>
2024-07-04 09:12:46 +0300

    Implement DELETE functionality for specific product

--- store/models.py
+++ store/models.py
@@ -90,7 +90,7 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)        # if we delete an Order to not delete OrderItem
-    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='orderitems')
     quantity = models.PositiveSmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)    # price of products can change over time so we should always store the price
 
